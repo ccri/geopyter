7948c7af7bd5ce350463c577385bffe71522bda5
Gerard Castro Briones <gbriones@ccri.com>
2017-01-27 15:56:18 -0500

    initial addition of d3.table

--- geopyter/core/vis_libs/d3/d3_generator.py
+++ geopyter/core/vis_libs/d3/d3_generator.py
@@ -1,10 +1,11 @@
-from geopyter.core.vis_libs.d3.vis import histogram
+from geopyter.core.vis_libs.d3.vis import histogram, table
 
 # dict of currently supported visualization types for D3js
 # key = visualization type
 # val = default make() function for visualization type
 vis_types = {
-    'histogram': histogram.make
+    'histogram': histogram.make,
+    'table': table.make
 }
 
 def create_visualization(data_path, vis_type, vis_params):
