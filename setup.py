16d1328423268ac305a333a564757a7b93e1ead6
Gerard Castro Briones <gbriones@ccri.com>
2017-02-07 10:53:30 -0500

    adjusting external lib imports for install

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from setuptools import setup, find_packages
 
-setup(name='geopyter',
-      version='0.0.1',
-      author='Gerard C. Briones',
-      author_email='gbriones@ccri.com',
-      packages=find_packages(),
-      install_requires=[
-            'ipython',
-            'jupyter',
-            'tornado'
-      ],
-      data_files=[
-          ('share/jupyter/nbextensions', [
-              'geopyter/geopyter.css',
-              'external/d3js/d3.min.js',
-              'external/leaflet/leaflet.css',
-              'external/leaflet/leaflet.js']),
-          ('share/jupyter/nbextensions/images', [
-              'external/leaflet/images/layers.png',
-              'external/leaflet/images/layers-2x.png',
-              'external/leaflet/images/marker-icon.png',
-              'external/leaflet/images/marker-icon-2x.png',
-              'external/leaflet/images/marker-shadow.png'])
-          ]
-      )
+setup(
+    name='geopyter',
+    version='0.0.1',
+    author='Gerard C. Briones',
+    author_email='gbriones@ccri.com',
+    packages=find_packages(),
+    install_requires=[
+        'ipython',
+        'jupyter',
+        'tornado'
+    ],
+    data_files=[
+        ('share/jupyter/nbextensions', [
+            'geopyter/geopyter.css',
+            'external/d3js/d3.min.js',
+            'external/leaflet/leaflet.css',
+            'external/leaflet/leaflet.js']),
+        ('share/jupyter/nbextensions/images', [
+            'external/leaflet/images/layers.png',
+            'external/leaflet/images/layers-2x.png',
+            'external/leaflet/images/marker-icon.png',
+            'external/leaflet/images/marker-icon-2x.png',
+            'external/leaflet/images/marker-shadow.png'])
+    ]
+)
