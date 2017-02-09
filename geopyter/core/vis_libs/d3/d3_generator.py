e368c1c135985abc14b0ae6537b5f5ee786785ef
Gerard Castro Briones <gbriones@ccri.com>
2017-02-09 11:26:19 -0500

    integration of time series visualization

--- geopyter/core/vis_libs/d3/d3_generator.py
+++ geopyter/core/vis_libs/d3/d3_generator.py
@@ -1,11 +1,12 @@
-from geopyter.core.vis_libs.d3.vis import histogram, table
+from geopyter.core.vis_libs.d3.vis import histogram, table, timeseries
 
 # dict of currently supported visualization types for D3js
 # key = visualization type
 # val = default make() function for visualization type
 vis_types = {
     'histogram': histogram.make,
-    'table': table.make
+    'table': table.make,
+    'timeseries': timeseries.make
 }
 
 def create_visualization(data_path, vis_type, vis_params):
